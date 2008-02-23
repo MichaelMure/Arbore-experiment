32248329a85fc881b8dfedfcb51bd3c0c74f50bf
lds <lds@f7b84f85-2e04-4de5-9e99-c4612dc0539c>
2008-02-23 20:42:50 +0000

    Script to add gpl headers
    git-svn-id: https://piggledy.org/svn/peerfuse/trunk@376 f7b84f85-2e04-4de5-9e99-c4612dc0539c

---
+++ tools/svn-bots/gpl-header.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Copyright(C) 2008 Laurent Defert, Romain Bignon
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+
+set -e
+
+cd /home/p2pfs-buildbot/peerfuse/gpl_header
+headers_count=0
+
+# Check cpp / h files
+for file in $(find -name \*.cpp) $(find -name \*.h)
+do
+	head -n 5 "$file" | grep -i Copyright > /dev/null && continue
+	cat tools/svn-bots/gpl_header_cpp "$file" > tmp
+	mv tmp "$file"
+	headers_count=$(($headers_count+1))
+done
+
+# Check sh files
+for file in $(find -name \*.sh)
+do
+	head -n 5 "$file" | grep Copyright > /dev/null && continue
+
+	cat tools/svn-bots/gpl_header_sh > tmp
+
+	if [ "$(head -n 1 "$file"|grep '#!/bin/')" != "" ]
+	then
+		# remove the #!/bin/bash
+		LINES=$(cat "$file"|wc -l)
+		echo $LINES
+		tail -n $(( $LINES - 1 )) "$file" >> tmp
+	else
+		cat "$file" >> tmp
+	fi
+
+	mv tmp "$file"
+	headers_count=$(($headers_count+1))
+done
+
+if [ "$header_count" != "0" ]
+then
+	svn ci -m "$headers_count GPL headers added"
+fi
+
