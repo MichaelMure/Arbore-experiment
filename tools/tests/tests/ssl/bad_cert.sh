9bb2ba46088a3c27240245a177200dd0c22dca5e
lds <lds@f7b84f85-2e04-4de5-9e99-c4612dc0539c>
2008-03-27 06:34:10 +0000

    Ssl basic tests
    git-svn-id: https://piggledy.org/svn/peerfuse/trunk@530 f7b84f85-2e04-4de5-9e99-c4612dc0539c

---
+++ tools/tests/tests/ssl/bad_cert.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source "$TESTS/bash/tests.sh"
+
+export LISTEN_PORT=9876
+export CONNECT_PORT=6789
+start_p2pfs
+
+SSL_LOG="$(echo blah| openssl s_client -connect localhost:9876 -key $TESTS/conf/certs/bad-key.key -cert $TESTS/conf/certs/bad-key.crt 2>&1 >/dev/null)"
+
+if [ "$(echo "$SSL_LOG"|tail -n 1)" == "DONE" ]
+then
+	echo "$SSL_LOG"
+	exit 1
+fi
+
+exit 0
