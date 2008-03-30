c03c39e46a2c4fd8e4703d755e28711adeb81382
romain <romain@f7b84f85-2e04-4de5-9e99-c4612dc0539c>
2008-03-30 13:52:45 +0000

    fix css url
    git-svn-id: https://piggledy.org/svn/peerfuse/trunk@563 f7b84f85-2e04-4de5-9e99-c4612dc0539c

--- tools/stats/commit-stats/stats.py
+++ tools/stats/commit-stats/stats.py
@@ -456,7 +456,7 @@ def main():
 <head>
     <title>Stats of commits</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel='stylesheet' href='file:///home/rom1/Projets/peerfuse/www/style.css' type='text/css' />
+    <link rel='stylesheet' href='http://peerfuse.org/style.css' type='text/css' />
     <link rel="shortcut icon" type="image/png" href="http://peerfuse.org/favicon.png" />
     <script type="text/javascript">
 
