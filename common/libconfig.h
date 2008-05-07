4d00b0f8d05848e875ad9dc6e75be0c82524a137
romain <romain@f7b84f85-2e04-4de5-9e99-c4612dc0539c>
2008-05-07 22:51:21 +0000

    added openssl licence in headers
    git-svn-id: https://piggledy.org/svn/peerfuse/trunk@1004 f7b84f85-2e04-4de5-9e99-c4612dc0539c

--- common/libconfig.h
+++ common/libconfig.h
@@ -16,6 +16,10 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * This product includes cryptographic software written by Eric Younganus
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
  * $Id$
  */
 
--- peerfuse-lan/pf_file.cpp
+++ peerfuse-lan/pf_file.cpp
@@ -14,5 +14,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  *
+ * This product includes cryptographic software written by Eric Younganus
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
  * $Id$
  */
