a9efcc3b809ecf30d83b9e6fa01df81f75f73be0
Michael Muré <batolettre@gmail.com>
2011-10-12 21:42:16 +0200

    add missing lib/files/file_perms.cpp

---
+++ lib/files/file_perms.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright(C) 2011 Michael Muré <batolettre@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ *
+ */
+
+#include "file_perms.h"
+
+FilePermissions::FilePermissions()
+	: pf_mode(0),
+	mode(S_IFREG | S_IRUSR | S_IWUSR),
+	uid(getuid()),
+	gid(getgid())
+	{}
