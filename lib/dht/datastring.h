e8ef449fb523b2f1ea6d44715aa298dbe23f1335
Benoit Saccomano <bainwa@gmail.com>
2012-01-14 18:24:08 +0100

    Add skeleton of DHT data-structures

---
+++ lib/dht/datastring.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright(C) 2012 Benoît Saccomano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ * This file contains some code from the Chimera's Distributed Hash Table,
+ * written by CURRENT Lab, UCSB.
+ *
+ */
+
+#ifndef DATASTRING_H
+#define DATASTRING_H
+
+#include "data.h"
+#include <set>
+#include <string>
+
+class DataString : public Data
+{
+public:
+	DataString(std::string name);
+	void add(std::string name);
+	void remove(std::string name);
+	int getSize() const;
+
+private:
+	std::set<std::string> nameList_;
+};
+
+#endif
---
+++ lib/dht/storage.h
@@ -0,0 +1,59 @@
+/*
+ * Copyright(C) 2012 Benoît Saccomano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ * This file contains some code from the Chimera's Distributed Hash Table,
+ * written by CURRENT Lab, UCSB.
+ *
+ */
+
+#ifndef STORAGE_H
+#define STORAGE_H
+
+#include <map>
+#include "data.h"
+#include <string>
+#include <util/key.h>
+
+class Storage
+{
+public:
+
+	Storage();
+	virtual ~Storage();
+	void addInfo(Key k, Key info);
+	void addInfo(Key k, std::string info);
+	void removeInfo(Key k, Key info);
+	void removeInfo(Key k, std::string info);
+	bool isKeyList(Key k) const;
+	bool isStringList(Key k) const;
+	void removeKey(Key k);
+	bool hasKey(Key k) const;
+	Data getInfo(Key k) const;
+	void clean();
+	void clear();
+
+private:
+	std::map<Key, Data> dataMap_;
+
+	};
+
+
+
+#endif
