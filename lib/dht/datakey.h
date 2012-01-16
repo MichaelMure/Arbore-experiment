4a8338b36bceb80bece125f2f5f4f749aadb6236
Benoit Saccomano <bainwa@gmail.com>
2012-01-16 14:18:44 +0100

    Datakey class added to the DHT storage skeleton

---
+++ lib/dht/datakey.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright(C) 2012 Benoît Saccomano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ * This file contains some code from the Chimera's Distributed Hash Table,
+ * written by CURRENT Lab, UCSB.
+ *
+ */
+
+#ifndef DATAKEY_H
+#define DATAKEY_H
+
+#include "data.h"
+#include <set>
+#include <util/key.h>
+
+class DataKey : public Data
+{
+public:
+	DataKey(Key k);
+	void add(Key k);
+	void remove(Key k);
+	int getSize() const;
+
+private:
+	std::set<Key> keyList_;
+};
+
+#endif
