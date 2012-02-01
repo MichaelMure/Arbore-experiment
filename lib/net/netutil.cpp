77c575239ccaeece5687f05a744bde92d703f5ff
Benoit Saccomano <bainwa@gmail.com>
2012-02-01 17:06:38 +0100

    Netutil: function for serialisation/deserialisation of basic type

---
+++ lib/net/netutil.cpp
@@ -0,0 +1,83 @@
+/*
+ * Copyright(C) 2012 Benoît Saccomano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ * This file contains some code from the Chimera's Distributed Hash Table,
+ * written by CURRENT Lab, UCSB.
+ *
+ */
+
+#include "netutil.h"
+#include <string>
+
+void Netutil::dump(uint32_t nbr, char* buff)
+{
+	nbr = htonl(nbr);
+	memcpy(buff, &nbr, sizeof(nbr));
+}
+
+size_t Netutil::size(uint32_t nbr)
+{
+	return sizeof(nbr);
+}
+
+void Netutil::dump(uint64_t nbr, char* buff)
+{
+	nbr = htonll(nbr);
+	memcpy(buff, &nbr, sizeof(nbr));
+}
+
+size_t Netutil::size(uint64_t nbr)
+{
+	return sizeof(nbr);
+}
+
+void Netutil::dump(const std::string& str, char* buff)
+{
+	uint32_t str_len = (uint32_t)str.size();
+	dump(str_len, buff);
+	memcpy(buff + size(str_len), str.c_str(), str.size());
+}
+
+size_t Netutil::size(std::string str)
+{
+	return str.size() + sizeof(uint32_t);
+}
+
+uint32_t Netutil::ReadInt32(char* buff)
+{
+	uint32_t val = ntohl(*(uint32_t*)buff);
+	return val;
+}
+
+uint64_t Netutil::ReadInt64(char* buff)
+{
+	uint64_t val = ntohll(*(uint64_t*)buff);
+	return val;
+}
+
+std::string Netutil::ReadStr(char* buff)
+{
+	uint32_t str_size = ReadInt32(buff);
+	char* str = new char [str_size+1];
+	memcpy(str, buff, str_size);
+	str[str_size] = '\0';
+	std::string val = std::string(str);
+	return val;
+}
---
+++ lib/net/netutil.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright(C) 2012 Benoît Saccomano
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 2 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * This product includes cryptographic software written by Eric Young
+ * (eay@cryptsoft.com).  This product includes software written by Tim
+ * Hudson (tjh@cryptsoft.com).
+ *
+ * This file contains some code from the Chimera's Distributed Hash Table,
+ * written by CURRENT Lab, UCSB.
+ *
+ */
+
+#ifndef DATA_H
+#define DATA_H
+
+#include <stdint.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <netinet/in.h>
+#include <util/tools.h>
+
+class Netutil
+{
+public:
+	// Writing to buffer functions
+	static void dump(uint32_t nbr, char* buff);
+	static void dump(uint64_t nbr, char* buff);
+	static void dump(const std::string& str, char* buff);
+
+	static size_t size(uint32_t nbr);
+	static size_t size(uint64_t nbr);
+	static size_t size(std::string str);
+
+	// Reading from buffer functions
+	static uint32_t ReadInt32(char* buff);
+	static uint64_t ReadInt64(char* buff);
+	static std::string ReadStr(char* buff);
+};
+
+#endif
